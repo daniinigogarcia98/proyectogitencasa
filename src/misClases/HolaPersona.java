c888210bdbf7647de1dd2c640a05455b5d7be25a
daniinigogarcia98 <149794707+daniinigogarcia98@users.noreply.github.com>
2023-11-12 13:37:13 +0100

    HolaPersona.java

--- src/misClases/HolaPersona.java
+++ src/misClases/HolaPersona.java
@@ -5,10 +5,11 @@ import javax.swing.JOptionPane;
 public class HolaPersona {
 
 	public static void main(String[] args) {
+		//JOptionPane sirve para darnos cuadros de dialogo en interfaz grafica en lugar de consola
 		String nombre = JOptionPane.showInputDialog("Dame tu Nombre: ");
         String apellidos=JOptionPane.showInputDialog("Dame tus Apellidos: ");
-		JOptionPane.showMessageDialog(null, "Hola ¿Que tal? " + nombre + " " + apellidos);
-
+		JOptionPane.showMessageDialog(null, "Hola Â¿Que tal? " + nombre + " " + apellidos);
+		
 	}
 
 }
