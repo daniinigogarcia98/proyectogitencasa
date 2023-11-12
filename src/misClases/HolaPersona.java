6616f73043d8b48701c38fe4e4e7a5c2a0a3abf9
daniinigogarcia98 <daniinigogarcia98@gmail.com>
2023-11-12 13:04:54 +0100

    modificandoclasepersona

--- src/misClases/HolaPersona.java
+++ src/misClases/HolaPersona.java
@@ -5,9 +5,9 @@ import javax.swing.JOptionPane;
 public class HolaPersona {
 
 	public static void main(String[] args) {
-		String nombre = JOptionPane.showInputDialog("Dame tu Nombre:");
-
-		JOptionPane.showMessageDialog(null, "Hola ¿Que tal? " + nombre);
+		String nombre = JOptionPane.showInputDialog("Dame tu Nombre: ");
+        String apellidos=JOptionPane.showInputDialog("Dame tus Apellidos: ");
+		JOptionPane.showMessageDialog(null, "Hola ¿Que tal? " + nombre + " " + apellidos);
 
 	}
 
